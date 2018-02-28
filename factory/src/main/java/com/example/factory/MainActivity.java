a3f0e3f87b1cdfb35c0dc91593932ec700844f23
Will <will.g.zhou@magugi.com>
2018-02-28 16:02:08 +0800

    commit

--- factory/src/main/java/com/example/factory/MainActivity.java
+++ factory/src/main/java/com/example/factory/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.factory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 工厂方法模式定义:定义一个用于创建对象的接口,让子类决定实例化那个类
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
